04d98529b9decebc7dfb46265c2066f2580c9f5e
Joshua Lovin <josh.lovin@revature.net>
2021-07-14 18:40:42 -0400

    Variant conditional blocks

--- Lab - ConditionalBlocks/src/ConditionalBlocks.java
+++ Lab - ConditionalBlocks/src/ConditionalBlocks.java
@@ -2,11 +2,20 @@
 public class ConditionalBlocks {
 	
 	public static void main(String[] args) {
-		boolean b = true;
-//		boolean b = false;
+		boolean firstCondition = false;
+		boolean secondCondition = true;
 		
-		if (b) {
+		if (firstCondition) {
 			System.out.println("inside the if-statement");
+			
+			if (secondCondition) {
+				System.out.println("inside the nested if statement");
+			}
+		
+		} else if (secondCondition){
+			System.out.println(5);
+		} else {
+			System.out.println("inside the else-statement");
 		}
 		
 		System.out.println("Outside the if-statement");
