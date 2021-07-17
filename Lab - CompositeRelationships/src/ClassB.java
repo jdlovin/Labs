af6440dcac38eba4e1f28cd41be4629cd5ceda2a
Josh Lovin <josh.lovin@revature.net>
2021-07-17 14:10:19 -0400

    writing composite classes

---
+++ Lab - CompositeRelationships/src/ClassB.java
@@ -0,0 +1,13 @@
+
+public class ClassB {
+	
+	ClassA classA = new ClassA();
+	
+	public static void main(String[] args) {
+		ClassB b = new ClassB();
+		b.classA.setName("taylor");
+		
+		System.out.println(b.classA.getName());
+	}
+
+}
