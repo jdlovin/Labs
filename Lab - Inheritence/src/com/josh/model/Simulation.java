f84ba33dd62e0dd2340327c553d1052f389b4361
Josh Lovin <josh.lovin@revature.net>
2021-07-20 18:15:41 -0400

    Inheritence part 2

---
+++ Lab - Inheritence/src/com/josh/model/Simulation.java
@@ -0,0 +1,14 @@
+package com.josh.model;
+
+public class Simulation {
+
+	public static void main(String[] args) {
+		FlyingVehicle fv = new FlyingVehicle((short)4, new Engine());
+		
+		fv.setName("Big Bertha");
+		System.out.println(fv.getName());
+		
+		Airplane boeing = new Airplane((short)4, (short)5);
+		boeing.move(15);
+	}
+}
